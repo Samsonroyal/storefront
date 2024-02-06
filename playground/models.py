98b212465ddb3311bbcf41e18e6821e861823674
Were Samson Bruno <werefast2000@gmail.com>
2024-02-06 11:12:45 +0000

    created Query Model

--- playground/models.py
+++ playground/models.py
@@ -5,3 +5,9 @@ class PriceHistory(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     volume = models.PositiveIntegerField()
+
+class Query(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
