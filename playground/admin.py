94ea5d5ee50f3695b4fad68181cdd0b610145f70
Were Samson Bruno <werefast2000@gmail.com>
2024-02-07 15:18:15 +0000

    Add CourseAdmin to admin.py

--- playground/admin.py
+++ playground/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'date_updated')
+
+admin.site.register(Course, CourseAdmin)
