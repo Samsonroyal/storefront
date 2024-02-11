97965560680f206130505492a9c2fce741bd7395
Were Samson Bruno <werefast2000@gmail.com>
2024-02-11 13:20:49 +0000

    added models to user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class IMUser(models.Model):
+    USER_TYPES = [
+        ('EIT', 'EIT'),
+        ('TEACHING_FELLOW', 'Teaching Fellow'),
+        ('ADMIN_STAFF', 'Admin Staff'),
+        ('ADMIN', 'Admin'),
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPES)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+class Cohort(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.CASCADE)
+
+
+
+class CohortMember(models.Model):
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+    member = models.ForeignKey(IMUser, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.CASCADE)
+
+
